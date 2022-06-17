fdfe7d934b5678d5866a34b918ca92f341937e4d
jessicak98 <jessica.kroneman@student.nhlstenden.com>
2022-06-17 12:32:00 +0200

    gebruiker delete werkt

--- Includes/deletegebruiker.php
+++ Includes/deletegebruiker.php
@@ -1,19 +1,11 @@
 <?php
-
 include_once ('./connection.php');
-//include_once("./Includes/links.php");
-
-if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['delete'])):	
-				   
-	$GebruikerMail = array_search("delete",$_POST['delete']);	 
 	
-	$sql = "DELETE FROM gebruikers WHERE GebruikerMail = :GebruikerMail";
-	$stmt = $pdo->prepare($sql);
-	$stmt->bindParam(':GebruikerMail', $GebruikerMail);
-	$stmt->execute();
+	$GebruikerID = $_GET['id'];
+	$sth = $conn->prepare("DELETE FROM gebruiker where GebruikerID = $GebruikerID");
+	$sth->execute();
+
 	header('Location: ./GebruikersBekijken.php');
-	exit;
-endif;
 
 ?>
  
