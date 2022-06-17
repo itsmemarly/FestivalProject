ca606e3d25731f193434b408f25d6820d54807ef
Marleen ( 玛琳) <handyyou@live.nl>
2022-06-17 10:59:51 +0200

    test

--- includes/festivalaanpassen.php
+++ includes/festivalaanpassen.php
@@ -1,5 +1,6 @@
 <?php
 include_once ('connection.php');
+include_once('links.php');
 $FestivalID = $_GET['id'];
 $sth = $conn->prepare("SELECT * FROM festival where FestivalID = $FestivalID");
 $sth->execute();
@@ -17,20 +18,6 @@ $FestivalEindtijd = $row['FestivalEindtijd'];
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
-    <!-- Bootstrap 5 -->
-    <link rel="stylesheet" href="/path/to/cdn/bootstrap.min.css" />
-    <script src="/path/to/cdn/bootstrap.bundle.min.js"></script>
-
-    <!-- jQuery -->
-    <script src="/path/to/cdn/jquery.slim.min.js"></script>
-
-    <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-            crossorigin="anonymous"></script>
-    
     <meta charset="UTF-8">
     <title>Title</title>
 </head>
@@ -62,13 +49,13 @@ include_once("./exclNavbar.php");
                     <div class=" ms-2 mb-3 row">
                         <label for="FestivalLogo" class="col-sm-12 col-form-label">Voeg logo toe:</label>
                         <div class="col-sm-8">
-                            <input class="form-control form-control-md" type="file" name="FestivalLogo" value="<?php echo $FestivalLogo?>">
+                            <input class="form-control form-control-md" type="file" name="FestivalLogo" value="<?php echo $FestivalLogo?>"></input>
                         </div>
                        
                     <div class="mt-4 ms-2 mb-3 row">
                         <label for="FestivalInfo" class="col-sm-12 col-form-label">Voeg een beschrijving toe:</label>
                         <div class="col-sm-8">
-                            <textarea class="form-control form-control-md"  name="FestivalInfo"  rows="3" value="<?php echo $FestivalInfo?>"></textarea>
+                            <textarea class="form-control form-control-md"  name="FestivalInfo"  rows="3" ><?php echo $FestivalInfo?></textarea>
                         </div>
                     </div>
                     <div class=" ms-2 mb-3 row">
