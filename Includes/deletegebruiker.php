698a101eedd40e97bd612ce541ed9c630db6c4f4
Patrick-Kuipers <91322797+Patrick-Kuipers@users.noreply.github.com>
2022-06-20 21:57:19 +0200

    sponsor + background

--- Includes/deletegebruiker.php
+++ Includes/deletegebruiker.php
@@ -3,17 +3,12 @@
 include_once ('./connection.php');
 //include_once("./Includes/links.php");
 
-if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['delete'])):	
-				   
-	$GebruikerMail = array_search("delete",$_POST['delete']);	 
-	
-	$sql = "DELETE FROM gebruikers WHERE GebruikerMail = :GebruikerMail";
-	$stmt = $pdo->prepare($sql);
-	$stmt->bindParam(':GebruikerMail', $GebruikerMail);
-	$stmt->execute();
-	header('Location: ./GebruikersBekijken.php');
-	exit;
-endif;
+$GebruikerID = $_GET['id'];
+$sth = $conn->prepare("DELETE FROM gebruiker where GebruikerID = $GebruikerID");
+$sth->execute();
+
+header('Location:./GebruikersBekijken.php');
+
 
 ?>
  
--- Includes/navbar.php
+++ Includes/navbar.php
@@ -1,6 +1,5 @@
 <nav class="navbar navbar-dark bg-dark">
         <div class="container-fluid">
-          <img src="https://lirp.cdn-website.com/a8be4524/dms3rep/multi/opt/logo+surventofestival-98w.png" alt="survento logo">
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
             data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
             aria-expanded="false" aria-label="Toggle navigation">
@@ -9,10 +8,10 @@
         </div>
         <div class="collapse" id="navbarToggleExternalContent">
             <div class="bg-dark p-4">
-                <a class="nav-link" style="color: aliceblue; font-size: 30px;" href="./index.php">Home</a>
-                <a class="nav-link" style="color: aliceblue; font-weight: bolder; font-size: 30px;" href="./inschrijven.php">Inschrijven</a>
-[                <a class="nav-link" style="color: aliceblue; font-size: 30px;" href="./sponsoren.php">Sponsoren</a>
-]                <a class="nav-link" style="color: aliceblue; font-size: 30px;" href="./login.php">Inloggen</a>
+                <a class="nav-link" style="color: aliceblue; font-weight: bolder; font-size: 30px;" href="./index.php">Home</a>
+                <a class="nav-link" style="color: aliceblue; font-size: 30px;" href="./inschrijven.php">Inschrijven</a>
+                <a class="nav-link" style="color: aliceblue; font-size: 30px;" href="./sponsoren.php">Sponsoren</a>
+                <a class="nav-link" style="color: aliceblue; font-size: 30px;" href="./login.php">Inloggen</a>
                 <a class="nav-link" style="color: aliceblue; font-size: 30px;" href="./Includes/dashboard.php">Dashboard</a>
             </div>
         </div>
